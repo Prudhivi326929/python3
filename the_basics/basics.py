570dd50d979bf7b3fa0ee8a787b33e8cab65b8ad
Srinivasa Rao Prudhivi <57983089+Prudhivi326929@users.noreply.github.com>
2019-12-03 22:23:27 -0600

    updated with type()

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,7 +1,12 @@
 import datetime
 x=print(datatime.datetime.now())
-print(x)
+#print(x)
 
 number = 10
 word = "hello"
 print(number, word)
+x = 10
+y = 10.5
+z = "string"
+
+print(type(x), type(y), type(z))
