7b9a2917dd3f801d4d8874b0ecb0cb298b8e99d7
Srinivasa Rao Prudhivi <57983089+Prudhivi326929@users.noreply.github.com>
2019-12-03 23:22:32 -0600

    list methods

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -10,6 +10,14 @@ y = 10.5
 z = "string"
 student_grades = [1,2,3]
 
-print(type(x), type(y), type(z), type(student_grades))
-print(list(range(1,10))) #[1,2,3,4,5,6,7,8,9]
-print(list(range(1,10,2))) #[1,3,5,7,9]
+#print(type(x), type(y), type(z), type(student_grades))
+#print(list(range(1,10))) #[1,2,3,4,5,6,7,8,9]
+#print(list(range(1,10,2))) #[1,3,5,7,9]
+
+#print(dir(list)) #lists all possible methods with type "list"
+
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum/length
+print(mean)
+print(student_grades.count(1))
