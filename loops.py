f6bda7c39d3cf33a8189970e168ced4e18fccee3
Srinivasa Rao Prudhivi <57983089+Prudhivi326929@users.noreply.github.com>
2019-12-09 04:35:20 -0600

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,2 +1,8 @@
 temp = [1,2,3,4,5.1,9]
 for i in temp:
+  if(type(i)==int and i>2):
+    print(i)
+
+stu_grades = ["Marry":9.0, "Sim":8.8, ""John:7.4]
+for i in stu_grades.items():#you can use keys() or values() to iterate
+  print(i)
