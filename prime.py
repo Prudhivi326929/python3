bc99b9d5cea0048187c93bd5d9d7d15cb81235b7
Srinivasa Rao Prudhivi <57983089+Prudhivi326929@users.noreply.github.com>
2025-01-23 23:02:41 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -2,7 +2,7 @@ a = int(input("Enter a number: "))
 if a == 0 or a == 1:
     print("It's not a prime number.")
 else:
-    for i in range(2,5):
+    for i in range(2,a):
         if a%i == 0:
             print("Not a prime.")
             break
